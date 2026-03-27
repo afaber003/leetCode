0ff6ee0709011b8c1c3118606f603ad40fd27197
Andy Faber <afaber003@gmail.com>
2026-03-27 00:35:42 -0400

    worked more on the sudoku one

--- sudokuSolver.cpp
+++ sudokuSolver.cpp
@@ -9,11 +9,39 @@ class Solution {
 public:
     void solveSudoku(Board& board) {
 
-
+        
 
     }
 
 private:
+
+    static unordered_set<char> getAvailableOptions(const Board& board, const int& row, const int& col) {
+        unordered_set<char> itemsLeft = { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
+        unordered_set<char> itemsUsed;
+
+        itemsUsed = charsInSquare(board, posToSquare(row, col));
+        for (auto& item : itemsUsed) {
+            itemsLeft.erase(item);
+        }
+        if (itemsLeft.size() == 0 || itemsLeft.size() == 1) return itemsLeft;
+
+        itemsUsed = charsInRow(board, row);
+        for (auto& item : itemsUsed) {
+            itemsLeft.erase(item);
+        }
+        if (itemsLeft.size() == 0 || itemsLeft.size() == 1) return itemsLeft;
+        
+        itemsUsed = charsInColumn(board, col);
+        for (auto& item : itemsUsed) {
+            itemsLeft.erase(item);
+        }
+        return itemsLeft;
+    }
+
+    static inline int posToSquare(const int& row, const int& col) {
+        const vector<vector<int>> rowToColMap = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
+        return rowToColMap[row / 3][col / 3];
+    }
 
     static bool isSolved(const Board& board) {
         for (int index = 0; index < 9; index++) {
@@ -24,7 +52,11 @@ private:
         return true;
     }
 
-    static bool isSquareSolved(const Board& board, int squareNumber) {
+    static inline bool isSquareSolved(const Board& board, int squareNumber) {
+        return charsInSquare(board, squareNumber).size() == 9;
+    }
+
+    static unordered_set<char> charsInSquare(const Board& board, int squareNumber) {
         vector<int> rows, columns;
 
         // rows
@@ -59,15 +91,23 @@ private:
                 columns = { 6, 7, 8 };
         }
 
-        return charsIncludedInSelection(board, rows, columns).size() == 9;
+        return charsIncludedInSelection(board, rows, columns);
     }
 
     static bool isRowSolved(const Board& board, int rowNumber) {
-        return charsIncludedInSelection(board, { rowNumber }, { 0, 1, 2, 3, 4, 5, 6, 7, 8 }).size() == 9;
+        return charsInRow(board, rowNumber).size() == 9;
+    }
+
+    static unordered_set<char> charsInRow(const Board& board, const int& rowNumber) {
+        return charsIncludedInSelection(board, { rowNumber }, { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
     }
 
     static bool isColumnSolved(const Board& board, int columnNumber) {
-        return charsIncludedInSelection(board, { 0, 1, 2, 3, 4, 5, 6, 7, 8 }, { columnNumber }).size() == 9;
+        return charsInColumn(board, columnNumber).size() == 9;
+    }
+
+    static unordered_set<char> charsInColumn(const Board& board, const int columnNumber) {
+        return charsIncludedInSelection(board, { 0, 1, 2, 3, 4, 5, 6, 7, 8 }, { columnNumber });
     }
 
     static unordered_set<char> charsIncludedInSelection(const Board& board, const vector<int>& rows, const vector<int>& columns) {
